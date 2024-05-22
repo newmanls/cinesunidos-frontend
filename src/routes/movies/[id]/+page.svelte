11bdf1aef572f525f10e7934b8d71cd917ecbf22
Newman Sánchez <newmanls@pm.me>
2024-05-22 13:40:44 -0400

    improve showtimes display

--- src/routes/movies/[id]/+page.svelte
+++ src/routes/movies/[id]/+page.svelte
@@ -3,6 +3,12 @@
     import BackdropHeader from "$lib/components/BackdropHeader.svelte";
     export let data;
     const { movie } = data;
+
+    function formatTime(time) {
+        let timeParts = time.split("T")[1].split(":");
+        console.log(timeParts);
+        return `${timeParts[0]}:${timeParts[1]}`;
+    }
 </script>
 
 <BackdropHeader {movie} />
@@ -10,16 +16,24 @@
     <section class="showtimes">
         <h2>Funciones</h2>
         {#each movie.theatres as theatre}
-            <h3>{theatre.name}</h3>
-            <ul>
-                {#each theatre.showtimes as showtime}
-                    <li>
-                        <a href="{base}/buy/{showtime.id}">
-                            {showtime.format} - {showtime.time} (Sala {showtime.auditorium})
-                        </a>
-                    </li>
-                {/each}
-            </ul>
+            <h4>{theatre.name}</h4>
+            {#each Object.entries(theatre.formats) as [format, showtimes]}
+                <table class="showtime-table">
+                    <tr>
+                        <td>{format}</td>
+                        <td>
+                            {#each showtimes as showtime}
+                                <a
+                                    role="button"
+                                    class="showtime-btn outline secondary"
+                                    href="{base}/buy/{showtime.id}"
+                                    >{formatTime(showtime.time)}</a
+                                >
+                            {/each}
+                        </td>
+                    </tr>
+                </table>
+            {/each}
         {:else}
             <p>No hay funciones disponibles para esta película</p>
         {/each}
@@ -30,14 +44,11 @@
     main {
         flex-grow: 3;
     }
-
-    .details {
-        font-size: 0.75rem;
+    .showtime-btn {
+        margin-right: 0.5rem;
+        padding: 0.25rem 0.5rem;
     }
-
-    .rating {
-        border: 1px gray solid;
-        padding: 0.1rem 0.5rem;
-        margin-right: 0.25rem;
+    table td:first-of-type {
+        width: 10rem;
     }
 </style>
